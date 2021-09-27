94e1731166a429ae3c9deb3185da2c3ce0f6a480
Andrew Reed <andrew_reed@hotmail.com>
2021-09-27 20:55:15 +0100

    adjusted tokens

--- build.gradle.kts
+++ build.gradle.kts
@@ -109,8 +109,8 @@ publishing {
             name = "GitHubPackages"
             url = uri("https://maven.pkg.github.com/gitliveapp/packages")
             credentials {
-                username = System.getenv("USERNAME")
-                password = System.getenv("TOKEN")
+                username = System.getenv("GITHUB_ACTOR")
+                password = System.getenv("GITHUB_TOKEN")
             }
         }
     }
